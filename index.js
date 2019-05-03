475680712d8fac1aa1573e666513b0ed4990716e
JoeLiang1983 <47882487+JoeLiang1983@users.noreply.github.com>
2019-05-03 14:42:47 +0800

    My Job is well done + Docker!

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.end("Hello World from another commit to Jenkins + Docker!");
+    response.end("My Job is well done + Docker!");
 });
 
 const port = process.env.PORT || 1337;
